94feb49a0b56540c20d59d02c1cae3e330c1e663
Maks Tulin <kotmatrosskin82@gmail.com>
2020-12-24 18:12:28 +0400

    fix: add heroku port

--- main_app/main.py
+++ main_app/main.py
@@ -1,6 +1,6 @@
 import configparser
 from aiohttp import web
-from os.path import join
+from os import path, environ
 from settings import ROOT_DIR, LOG_FILE, CONFIG
 from app_log import get_logger
 from routes import routes
@@ -8,16 +8,21 @@ from shared import user_agent
 
 
 def main():
-    config_path = join(ROOT_DIR, CONFIG)
+    config_path = path.join(ROOT_DIR, CONFIG)
     config = configparser.ConfigParser()
     config.read(config_path)
 
+    ON_HEROKU = environ.get('ON_HEROKU')
+    
     SRV_HOST = config.get("server", "host")
     SRV_PORT = config.get("server", "port")
+    if ON_HEROKU:
+        # get the heroku port 
+        SRV_PORT = int(os.environ.get("PORT", SRV_PORT))  # as per OP comments default is SRV_PORT
 
     user_agent.update()
 
-    log = get_logger(join(ROOT_DIR, LOG_FILE))
+    log = get_logger(path.join(ROOT_DIR, LOG_FILE))
 
     app = web.Application(logger=log)
     for route in routes:
